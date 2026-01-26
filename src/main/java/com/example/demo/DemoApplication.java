0eccec1ad235b9603b54be4a0611af96d158a9d5
Senthil Kumar <senthilkumar.8939096117@gmail.com>
2026-01-26 18:16:27 +0530

    cinnected with the postgresql

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,52 @@
+
 package com.example.demo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/api")
 public class DemoApplication {
 
+	@Autowired
+	UserRepository repo;
+
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(e ->
+				System.setProperty(e.getKey(), e.getValue())
+		);
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@GetMapping("/api/hello")
 	public String hello() {
-		return "Hello from your API!";
+		return "Hello from your API";
+	}
+
+	// Get all users
+	@GetMapping("/users")
+	public List<User> getUsers() {
+		return repo.findAll();
+	}
+
+	// Create user
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user) {
+		return repo.save(user);
 	}
 }
