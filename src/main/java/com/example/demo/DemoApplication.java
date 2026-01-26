920b930a3511bb32992cebd468ddc72e1a013def
Senthil Kumar <senthilkumar.8939096117@gmail.com>
2026-01-26 18:51:56 +0530

    Remove .env from tracking

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -22,13 +22,13 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.configure()
-				.ignoreIfMissing()
-				.load();
-
-		dotenv.entries().forEach(e ->
-				System.setProperty(e.getKey(), e.getValue())
-		);
+//		Dotenv dotenv = Dotenv.configure()
+//				.ignoreIfMissing()
+//				.load();
+//
+//		dotenv.entries().forEach(e ->
+//				System.setProperty(e.getKey(), e.getValue())
+//		);
 
 		SpringApplication.run(DemoApplication.class, args);
 	}
